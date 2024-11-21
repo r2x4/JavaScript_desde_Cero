d9b057058557c8a97603f3ed02f4d7474168c0fd
rodrigo <rrs230780@gmail.com>
2024-11-20 19:14:25 -0500

    Segundo

---
+++ 03-Control de Flujo/10_swich.js
@@ -0,0 +1,12 @@
+let accion = 'actualizar';
+
+switch (accion){
+    case 'listar':
+        console.log('Accion de Listar');
+        break;
+    case 'Guardar':
+        console.log('Accion de Guardar');
+        break;
+    default:
+        console.log('Accion no Reconocida');
+}
